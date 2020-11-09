a89c094eb8c5d2eb82397ebee11a6ab86ef40454
Melissa <melissakheying@gmail.com>
2020-11-08 18:18:11 -0600

    :construction: Create First Component, ServerComponent
    This is our first component called ServerComponent. We first need to import Component from @angular/core to use it's @Component decorator. The @Component decorator is a TypeScript feature that enhances your classes and enhances elements in your code. We use the @Component's functionality by identifying the ServerComponents selector name and set up it's HTML template.

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,12 @@
+// import the Component Functionality from Angular - core
+import { Component } from '@angular/core';
+
+// Use Component functionality to identify it's selector name
+// and set the ServerComponent's HTML with templateUrl
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html'
+})
+export class ServerComponent {
+
+}
